cfb57b9b135e24c716d22548f0792477c18e3b54
mikemannix92 <31800894+mikemannix92@users.noreply.github.com>
2017-09-23 11:29:58 -0400

    Add files via upload

--- Checker_Board.py
+++ Checker_Board.py
@@ -3,10 +3,9 @@ board=[]
 for i in range(8):
 	board.append(["o"]*8)
 
-#for i in board:
-#	print i
-
 def grid(insert):
 	for i in board:
 		print " ".join(i)
 print grid(board)
+
+#How to include the player pieces???
