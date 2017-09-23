09117ee38ccab651eec4d2dbc6e212dab185e485
mikemannix92 <31800894+mikemannix92@users.noreply.github.com>
2017-09-23 15:11:19 -0400

    Add files via upload

--- Checker_Board.py
+++ Checker_Board.py
@@ -1,11 +1,36 @@
 board=[]
 
 for i in range(8):
-	board.append(["o"]*8)
+	board.append(["-"]*8)
 
 def grid(insert):
 	for i in board:
 		print " ".join(i)
 print grid(board)
 
-#How to include the player pieces???
+#CPU Pieces
+
+for i in range(0,8,2):
+	board[0][i]="x"
+
+for i in range(1,8,2):
+	board[1][i]="x"
+
+for i in range(0,8,2):
+	board[2][i]="x"
+
+#Human Player Pieces
+
+for i in range(1,8,2):
+	board[5][i]="o"
+
+for i in range(0,8,2):
+	board[6][i]="o"
+
+for i in range(1,8,2):
+	board[7][i]="o"
+
+
+print grid(board)
+
+#Board is done....For Now......
